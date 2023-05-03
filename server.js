32afee000b6149ce02b8596fbcea04a852c452e3
Tyler Day <tmd1248@gmail.com>
2023-05-03 13:48:54 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+const swaggerUI = require('swagger-ui-express'), 
+  swaggerDocument = require('./swagger-output.json');
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+app.use(
+    '/api-docs', 
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerDocument)
+  );
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
